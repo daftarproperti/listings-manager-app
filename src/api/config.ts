81812f86813b13e362fc51b4c8544d6ae0d4d139
Kristo <ardiankristo16@gmail.com>
2024-01-10 19:06:04 +0700

    fix: auth flow

--- src/api/config.ts
+++ src/api/config.ts
@@ -3,7 +3,7 @@ import axios, { AxiosError } from 'axios'
 const tele = window.Telegram?.WebApp
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
 axios.defaults.headers.common['X-INIT-DATA'] =
-  import.meta.env.VITE_X_INIT_DATA && tele?.initData
+  import.meta.env.VITE_X_INIT_DATA || tele?.initData
 
 axios.interceptors.response.use(null, (error: AxiosError) => {
   if (error.response?.status === 403) {
--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,17 +1,4 @@
-import { useGetPropertyList } from 'api/queries'
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-
 const ErrorPage = () => {
-  const navigate = useNavigate()
-  const { isSuccess } = useGetPropertyList()
-
-  useEffect(() => {
-    if (isSuccess) {
-      navigate('/', { replace: true })
-    }
-  }, [isSuccess, navigate])
-
   return (
     <main className="flex min-h-screen w-full items-center justify-center">
       <h1>Authentication Error</h1>
