2ae85de14ad5d62c7f645ad6dcf4f74b68fe40c7
Sonny Sasaka <sonnysasaka@gmail.com>
2024-02-12 16:55:43 -0800

    Fix OCD title too close to border
    Change-Id: I8907fb14a4e9c48d67127d18a5fc741a53db7384

--- src/pages/list/Card.tsx
+++ src/pages/list/Card.tsx
@@ -16,7 +16,7 @@ const Card = ({ data }: { data: Property }) => {
         {!!data.pictureUrls?.length && (
           <ImageWithAuth link={data.pictureUrls[0]} />
         )}
-        <div className="flex flex-1 flex-col px-3 py-0.5">
+        <div className="flex flex-1 flex-col px-3 py-2">
           <div className="text-xs leading-4 text-slate-500">{data.title}</div>
           <div className="mt-2 flex flex-col">
             <div className="text-2xl font-semibold leading-8 text-slate-800">
