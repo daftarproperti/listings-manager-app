593bb11e16efdeeefd1df0436de4c6a0ed32b0b3
Sonny Sasaka <sonny@jelajahrumah.id>
2024-03-23 15:12:27 -0700

    Polish share button behavior
    * Don't show "successfully shared" toast when sharing, since the user
      may only copy the link.
    * Try navigator.share first, if fails try copy clipboard.
    
    Change-Id: Ibf0afa128cc488595cb5ba13f0968c7c886184cf

--- src/components/button/ShareButton.tsx
+++ src/components/button/ShareButton.tsx
@@ -16,26 +16,38 @@ const ShareListingsButton = ({
   className,
 }: ShareButtonProps) => {
   const handleShare = async () => {
+    // Try navigator.share first, if available and has permission.
     if (navigator.share) {
       try {
         await navigator.share({
           title,
           url,
         })
-        toast('Content shared successfully', { type: 'success' })
-      } catch (error) {
-        toast(`Error sharing content ${error}`, { type: 'error' })
+        // The user may just choose "copy link", so don't show "shared successfully" toast here
+        // to not mislead the user.
+        return
+      } catch (error: unknown) {
+        if (error instanceof Error && error.name === 'AbortError') {
+          // Not actually an error, but the user just cancelled it, so just return and don't show
+          // any toast.
+          return
+        }
+        if (!(error instanceof Error && error.name === 'NotAllowedError')) {
+          toast(`Error sharing content ${error}`, { type: 'error' })
+          return
+        }
       }
-    } else {
-      navigator.clipboard
-        .writeText(url)
-        .then(() => {
-          toast(`Successfully copied to clipboard`, { type: 'info' })
-        })
-        .catch((err) => {
-          toast('Could not copy text: ', err)
-        })
     }
+
+    // navigator.share is either not available or does not have permission, do clippboard.
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        toast(`Successfully copied to clipboard`, { type: 'info' })
+      })
+      .catch(() => {
+        toast('Could not copy text', { type: 'error' })
+      })
   }
 
   return (
