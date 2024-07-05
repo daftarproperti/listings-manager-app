4cc865964b934baabf037f32578e59ca485f71a1
Kristo <ardiankristo16@gmail.com>
2024-07-05 12:26:49 +0700

    fix: intuitive price field bug
    Change-Id: Ic7474017803c26ce25e0bd6c694d36d105cda608

--- src/components/input/IntuitiveCurrencyInputField.tsx
+++ src/components/input/IntuitiveCurrencyInputField.tsx
@@ -24,19 +24,38 @@ const customAbbreviations: Record<Abbreviation, number> = {
   t: 1000000000000,
 }
 
-export const convertAbbreviationToNumber = (input: string): string | number => {
+const convertAbbreviationToNumber = (input: string): string => {
   const match = input.toLowerCase().match(/([\d.,]*\d)\s*([a-zA-Z]*)?/)
 
   if (match) {
-    const numberPart = match[1]?.replace(/\./g, '').replace(/,/g, '.')
     const abbreviation = match[2]?.trim() as Abbreviation | undefined
 
     if (abbreviation && customAbbreviations[abbreviation]) {
       const multiplier = customAbbreviations[abbreviation]
-      const parsedNumber = parseFloat(numberPart)
-      if (!isNaN(parsedNumber)) {
-        return parsedNumber * multiplier
+      let numberString = match[1].replace(/,/g, '.')
+
+      // Remove any potential leading zeros
+      numberString = numberString.replace(/^0+/, '') || '0'
+
+      let numberValue
+      if (numberString.includes('.')) {
+        // Handle decimal part
+        const [integerPart, decimalPart] = numberString.split('.')
+
+        // Remove trailing zeros from the decimal part
+        const trimmedDecimalPart = decimalPart.replace(/0+$/, '')
+        const trimmedDecimalPlaces = trimmedDecimalPart.length
+
+        // Convert to BigInt and scale by the multiplier
+        numberValue =
+          BigInt(integerPart) * BigInt(multiplier) +
+          BigInt(trimmedDecimalPart) *
+            (BigInt(multiplier) / BigInt(Math.pow(10, trimmedDecimalPlaces)))
+      } else {
+        numberValue = BigInt(numberString) * BigInt(multiplier)
       }
+
+      return numberValue.toString()
     } else {
       return input
     }
@@ -81,11 +100,11 @@ const IntuitiveCurrencyInputField: React.FC<
             placeholder={placeholderValue}
             value={value ? formatNumber(value) : ''}
             onChange={(e) => {
-              const rawValue = e.target.value.replace(/\./g, '')
+              const rawValue = e.target.value.replace(/[Rp. \s]/g, '')
               onChange(rawValue)
             }}
             onBlur={(e) => {
-              const rawValue = e.target.value.replace(/\./g, '')
+              const rawValue = e.target.value.replace(/[Rp. \s]/g, '')
               onChange(convertAbbreviationToNumber(rawValue))
             }}
             className="mt-1 w-full items-start justify-center self-stretch whitespace-nowrap rounded-lg border border-solid border-[color:var(--royal-blue-200,#C6CAFF)] bg-white px-3 py-2.5 text-lg leading-7 text-gray-800"
