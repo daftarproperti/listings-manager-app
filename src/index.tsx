07a5fc9216dfa380d54cca9e635200a9c06f666c
Sonny Sasaka <sonny@jelajahrumah.id>
2024-09-27 23:36:46 -0700

    Remove unused dependencies
    Change-Id: Ia1e27e10b89299ebf2f00ef611e30a5b2f09c744

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,11 @@
 import App from 'pages/App'
 import { createRoot } from 'react-dom/client'
 
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import 'tailwindcss/tailwind.css'
 import './api/config'
 import './reactGA4/config'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
-
-if (import.meta.env.VITE_SERVICE_WORKER) {
-  serviceWorkerRegistration.register()
-}
 
 root.render(<App />)
