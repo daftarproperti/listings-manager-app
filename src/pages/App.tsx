87c336b34a2236598a4d2a44e36d9a27ab448efc
Sonny Sasaka <sonny@jelajahrumah.id>
2024-10-05 11:07:45 -0700

    style: always show vertical scrollbar
    To avoid "jumping" user experience when switching routes, always show
    the vertical scrollbar regardless of page height.
    
    Change-Id: Ifba24a58ab7d9b02c95f350fa8e064446a8ec3b9

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -14,7 +14,7 @@ const queryClient = new QueryClient()
 const App = () => (
   <>
     <BackButton />
-    <main className="mx-auto h-auto min-h-screen w-full bg-white font-inter text-slate-800">
+    <main className="mx-auto h-auto min-h-screen w-full overflow-y-scroll bg-white font-inter text-slate-800">
       <ThemeProvider>
         <QueryClientProvider client={queryClient}>
           <DirtyProvider>
